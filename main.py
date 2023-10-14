c2685b090a2f39968f28a87ec18033a9cb2d6a82
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-14 19:23:25 -0400

    Generate random walk

--- main.py
+++ main.py
@@ -15,16 +15,27 @@ tim.color("red")
 #     tim.forward(10)
 #     tim.down()
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        tim.forward(100)
-        tim.right(angle)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
+
+
+# for shape_side_num in range(3, 11):
+#     tim.color(random.choice(colors))
+#     draw_shape(shape_side_num)
 
 colors = ['blue', 'red', 'green', 'purple', 'orange', 'yellow']
-for shape_side_num in range(3, 11):
+directions = [0, 90, 180, 270]
+tim.speed("slow")
+tim.pensize(10)
+for _ in range(100):
     tim.color(random.choice(colors))
-    draw_shape(shape_side_num)
+    tim.setheading(random.choice(directions))
+    tim.forward(30)
+
+
 
 screen = Screen()
 screen.exitonclick()
