84372de21fe34dfbe59bacb6e2f723cda70bac13
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-14 19:35:32 -0400

    Generate Random RGB Colors with pyton tuples

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+import turtle as t
 import random
 
-tim = Turtle()
+tim = t.Turtle()
+t.colormode(255)
 tim.shape("turtle")
 tim.color("red")
+
 
 # for _ in range(4):
 #     tim.forward(100)
@@ -26,16 +29,22 @@ tim.color("red")
 #     tim.color(random.choice(colors))
 #     draw_shape(shape_side_num)
 
-colors = ['blue', 'red', 'green', 'purple', 'orange', 'yellow']
+# colors = ['blue', 'red', 'green', 'purple', 'orange', 'yellow']
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    return (r, g, b)
+
+
 directions = [0, 90, 180, 270]
 tim.speed("slow")
 tim.pensize(10)
 for _ in range(100):
-    tim.color(random.choice(colors))
+    tim.color(random_color())
     tim.setheading(random.choice(directions))
     tim.forward(30)
 
-
-
-screen = Screen()
+screen = t.Screen()
 screen.exitonclick()
